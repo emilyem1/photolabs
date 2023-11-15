eb497e6d4619b232a3ab9ab0e102ea1ad723391e
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-14 22:03:17 -0800

    Move object to App.jsx. Update function to use props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,30 +1,17 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   return (
   <div className="PhotoItem">
-    <img src={sampleDataForPhotoListItem.imageSource} alt="Photo" />
+    <img src={props.imageSource} alt="Photo" />
     <div className="UserInfo">
-      <img src={sampleDataForPhotoListItem.profile} alt="Profile" />
+      <img src={props.profile} alt="Profile" />
       <div className="UserDetails">
-        <p>{sampleDataForPhotoListItem.username}</p>
+        <p>{props.username}</p>
         <p>
-          {sampleDataForPhotoListItem.location.city},{" "}
-          {sampleDataForPhotoListItem.location.country}
+          {props.location.city},{" "}
+          {props.location.country}
         </p>
       </div>
     </div>
