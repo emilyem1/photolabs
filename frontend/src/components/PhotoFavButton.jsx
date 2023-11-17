8a6b17bb48d553065160f66bdd182a91a5efdb97
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-16 21:35:46 -0800

    Add logic for onClick event to turn FavIcon red or white

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,14 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [fave, setFave] = useState("notFave");
+  const favourite = () => {
+    setFave(fave === "favourite" ? "notFave" : "favourite");
+  }
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div className="photo-list__fav-icon-svg" onClick={favourite}>
+          <FavIcon selected={fave === "favourite"}/>
       </div>
     </div>
   );
