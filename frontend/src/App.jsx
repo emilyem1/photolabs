f6b06d6436914ca48a5e7589728234b47072b884
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-17 13:56:20 -0800

    Import topics and photos objects, add HomeRoute, pass objects into HR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import PhotoList from './components/PhotoList';
-import TopNavigationBar from './components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 import './App.scss';
+import topics from 'mocks/topics';
+import photos from 'mocks/photos';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigationBar />
-      <PhotoList />
+      <HomeRoute topics={topics} photos={photos}/>
     </div>
   );
 };
