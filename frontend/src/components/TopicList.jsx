be0132f570d7001bbd6d795cf33c7fc136fd2578
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-17 13:53:30 -0800

    Update props deconstruction

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,36 +3,9 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-  {
-    id: "4",
-    slug: "topic-4",
-    title: "Animals",
-  },
-  {
-    id: "5",
-    slug: "topic-5",
-    title: "Fashion",
-  },
-];
-
-const TopicList = () => {
-  const topic = sampleDataForTopicList.map((details) => {
+const TopicList = (props) => {
+  const { topics } = props;
+  const topic = topics.map((details) => {
     return <TopicListItem
       key={details.id}
       data={details}
