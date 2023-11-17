7905ac7beae5d31bee9ee61637d8b5afdc484177
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-16 20:57:26 -0800

    Destructure prop object to clean code

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
+  const { location, imageSource, username, profile } = props; // Destructure
   return (
   <div className="photo-list__item">
 
-    <img src={props.imageSource} alt="Photo" className="photo-list__image" />
+    <img src={imageSource} alt="Photo" className="photo-list__image" />
 
     <div className="photo-list__user-details">
 
-      <img src={props.profile} alt="Profile" className="photo-list__user-profile" />
+      <img src={profile} alt="Profile" className="photo-list__user-profile" />
 
       <div className="photo-list__user-info">
         <div className="photo-list__user-username">
-          <p>{props.username}</p>
+          <p>{username}</p>
         </div>
         <div className="photo-list__user-location">
           <p>
-            {props.location.city},{" "}
-            {props.location.country}
+            {location.city},{" "}
+            {location.country}
           </p>
         </div>
       </div>
