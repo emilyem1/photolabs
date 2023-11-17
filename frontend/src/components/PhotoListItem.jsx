f78324becf89fe1ef8f630ba73976e3187f4bce3
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-16 18:13:01 -0800

    Add className to render scss styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,16 +3,24 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-  <div className="PhotoItem">
-    <img src={props.imageSource} alt="Photo" />
-    <div className="UserInfo">
-      <img src={props.profile} alt="Profile" />
-      <div className="UserDetails">
-        <p>{props.username}</p>
-        <p>
-          {props.location.city},{" "}
-          {props.location.country}
-        </p>
+  <div className="photo-list__item">
+
+    <img src={props.imageSource} alt="Photo" className="photo-list__image" />
+
+    <div className="photo-list__user-details">
+
+      <img src={props.profile} alt="Profile" className="photo-list__user-profile" />
+
+      <div className="photo-list__user-info">
+        <div className="photo-list__user-username">
+          <p>{props.username}</p>
+        </div>
+        <div className="photo-list__user-location">
+          <p>
+            {props.location.city},{" "}
+            {props.location.country}
+          </p>
+        </div>
       </div>
     </div>
   </div>
