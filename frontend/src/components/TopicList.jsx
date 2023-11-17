5f8ba5dcbaf8353cde6fdca4ef18e6aa108d395f
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-17 12:29:01 -0800

    Add logic to render all the TopicListItems

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
   {
@@ -18,12 +19,28 @@ const sampleDataForTopicList = [
     slug: "topic-3",
     title: "People",
   },
+  {
+    id: "4",
+    slug: "topic-4",
+    title: "Animals",
+  },
+  {
+    id: "5",
+    slug: "topic-5",
+    title: "Fashion",
+  },
 ];
 
 const TopicList = () => {
+  const topic = sampleDataForTopicList.map((details) => {
+    return <TopicListItem
+      key={details.id}
+      data={details}
+    />;
+  });
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {topic}
     </div>
   );
 };
