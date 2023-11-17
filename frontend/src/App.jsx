cab4b85700c075f1e7361e85a571987bc9b629e4
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-17 12:42:50 -0800

    Add TopNavigationBar, remove TopicList to become a child of TNB

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import PhotoList from './components/PhotoList';
-import TopicList from 'components/TopicList';
+import TopNavigationBar from './components/TopNavigationBar';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-      <TopicList />
+      <TopNavigationBar />
       <PhotoList />
     </div>
   );
