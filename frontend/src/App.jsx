1e33f031956c49b757e816488ab4fc362b5e5996
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-17 12:29:59 -0800

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PhotoList from './components/PhotoList';
+import TopicList from 'components/TopicList';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="App">
       {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+      <TopicList />
       <PhotoList />
     </div>
   );
