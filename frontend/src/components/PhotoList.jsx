eca0e8541855fe2864e09d6d19d9de879d9487ec
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-17 17:20:50 -0800

    Pass favourite, setFavourite down through props

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,11 +4,13 @@ import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
-  const { photos } = props;
+  const { photos, favourite, setFavourite} = props;
   const photo = photos.map((details) => {
     return <PhotoListItem 
       key={details.id}
       data={details}
+      favourite={favourite}
+      setFavourite={setFavourite}
     />;
   });
   return <ul className="photo-list">{photo}</ul>
