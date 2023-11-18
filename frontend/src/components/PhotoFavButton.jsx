a94aae681544a796777cfc9a41de5f0633538270
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-17 17:20:18 -0800

    Add addFavourite function to execute on onClick

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,14 +3,16 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+  const { addFavourite } = props;
   const [fave, setFave] = useState("notFave");
-  const favourite = () => {
+  const handleClick = () => {
     setFave(fave === "favourite" ? "notFave" : "favourite");
+    addFavourite();
   }
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg" onClick={favourite}>
+      <div className="photo-list__fav-icon-svg" onClick={handleClick}>
           <FavIcon selected={fave === "favourite"}/>
       </div>
     </div>
