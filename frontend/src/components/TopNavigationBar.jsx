c0b205336e0b6fb439e7425e1f5ffa0875ff483b
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-17 17:30:07 -0800

    Pass isFavPhotoExist to FavBadge

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -6,12 +6,12 @@ import FavBadge from 'components/FavBadge';
 
 
 const TopNavigation = (props) => {
-  const { topics } = props;
+  const { topics, isFavPhotoExist } = props;
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics}/>
-      <FavBadge />
+      <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
   )
 }
