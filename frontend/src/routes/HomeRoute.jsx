10a5f60f9b736d66479cf2e24845eb336ea808b5
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-17 17:18:10 -0800

    Add useState to pass into PhotoList

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 import '../styles/HomeRoute.scss';
+import { useState } from 'react';
 import PhotoList from '../components/PhotoList';
 import TopNavigationBar from '../components/TopNavigationBar';
 
 const HomeRoute = (props) => {
   const { topics, photos } = props;
+  const [favourite, setFavourite] = useState([]);
   return (
     <div className="home-route">
-      <TopNavigationBar topics={topics}/>
-      <PhotoList photos={photos}/>
+      <TopNavigationBar topics={topics} />
+      <PhotoList 
+        photos={photos}
+        favourite={favourite}
+        setFavourite={setFavourite}
+      />
     </div>
   );
 };
