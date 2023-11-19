2605c8d4817c91bfcb28b84b4ae0a4438608831f
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-19 15:53:45 -0800

    Move useState for favourite, and addFavourite function to App. Change addFavourite to make it reusable. Pass both down

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,10 +13,34 @@ const App = () => {
   const toggleModal = () => {
     setShowModal(!showModal)
   }
+  const [favourite, setFavourite] = useState([]);
+  const addFavourite = (data) => {
+    const isExisting = favourite.includes(data.id);
+    if (isExisting) {
+      // If data.id exists, remove it from the array
+      const updatedFavourites = favourite.filter((id) => id !== data.id);
+      setFavourite(updatedFavourites);
+      console.log(`Removed ${data.id} from favourites: ${updatedFavourites}`);
+    } else {
+      setFavourite([...favourite, data.id])
+      console.log(`Added ${data.id} to favourites: ${[...favourite, data.id]}`);
+    }
+  }
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos} toggleModal={toggleModal} setSelectedPhoto={setSelectedPhoto} />
-      {showModal && <PhotoDetailsModal toggleModal={toggleModal} selectedPhoto={selectedPhoto} />} {/* If showModal is true, display modal */}
+      <HomeRoute 
+        topics={topics} 
+        photos={photos} 
+        toggleModal={toggleModal} 
+        setSelectedPhoto={setSelectedPhoto}
+        favourite={favourite}
+        addFavourite={addFavourite} 
+      />
+      {showModal && <PhotoDetailsModal 
+        toggleModal={toggleModal} 
+        selectedPhoto={selectedPhoto}
+        addFavourite={addFavourite}
+      />} {/* If showModal is true, display modal */}
     </div>
   );
 };
