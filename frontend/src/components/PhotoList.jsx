7e54fc681c3283383bf00e247bf6f00b36f8552a
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-18 19:11:49 -0800

    Pass down toggleModal

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,13 +4,14 @@ import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
-  const { photos, favourite, setFavourite} = props;
+  const { photos, favourite, setFavourite, toggleModal } = props;
   const photo = photos.map((details) => {
     return <PhotoListItem 
       key={details.id}
       data={details}
       favourite={favourite}
       setFavourite={setFavourite}
+      toggleModal={toggleModal}
     />;
   });
   return <ul className="photo-list">{photo}</ul>
