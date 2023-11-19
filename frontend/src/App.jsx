862afc4559bfc9252e8df61f8b888b7a99724d62
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-18 19:15:23 -0800

    Add logic to toggle the modal, create conditional to render it, pass down the function to toggle the state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import HomeRoute from 'routes/HomeRoute';
 import './App.scss';
 import topics from 'mocks/topics';
 import photos from 'mocks/photos';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    setShowModal(!showModal)
+  }
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos}/>
+      <HomeRoute topics={topics} photos={photos} toggleModal={toggleModal}/>
+      {showModal && <PhotoDetailsModal toggleModal={toggleModal} />} {/* If showModal is true, display modal */}
     </div>
   );
 };
