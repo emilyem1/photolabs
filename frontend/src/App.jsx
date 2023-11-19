088b9750889b9078427fe52b6e1e9214ed92bc91
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-18 23:45:32 -0800

    Add logic to grab data of clicked on PhotoListItem, pass down components of useState

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,13 +9,14 @@ import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const [showModal, setShowModal] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
   const toggleModal = () => {
     setShowModal(!showModal)
   }
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos} toggleModal={toggleModal}/>
-      {showModal && <PhotoDetailsModal toggleModal={toggleModal} />} {/* If showModal is true, display modal */}
+      <HomeRoute topics={topics} photos={photos} toggleModal={toggleModal} setSelectedPhoto={setSelectedPhoto} />
+      {showModal && <PhotoDetailsModal toggleModal={toggleModal} selectedPhoto={selectedPhoto} />} {/* If showModal is true, display modal */}
     </div>
   );
 };
