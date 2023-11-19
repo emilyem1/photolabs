2438f37f2317dfefd8317ad4d287aee6c4a1fab8
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-19 15:48:55 -0800

    Pass addFavourite function down PhotoList

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 
 import '../styles/HomeRoute.scss';
-import { useState } from 'react';
 import PhotoList from '../components/PhotoList';
 import TopNavigationBar from '../components/TopNavigationBar';
 
 const HomeRoute = (props) => {
-  const { topics, photos, toggleModal, setSelectedPhoto } = props;
-  const [favourite, setFavourite] = useState([]);
+  const { topics, photos, toggleModal, setSelectedPhoto, favourite, addFavourite } = props;
   return (
     <div className="home-route">
       <TopNavigationBar topics={topics} isFavPhotoExist={favourite.length > 0}/>
       <PhotoList 
         photos={photos}
-        favourite={favourite}
-        setFavourite={setFavourite}
         toggleModal={toggleModal}
         setSelectedPhoto={setSelectedPhoto}
+        addFavourite={addFavourite} 
       />
     </div>
   );
