64c8e52f1d95abe996a49c7de05cee129a20f43e
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-19 22:56:12 -0800

    Rework using new props

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,26 +6,29 @@ import PhotoFavButton from "components/PhotoFavButton";
 import PhotoList from 'components/PhotoList';
 
 const PhotoDetailsModal = (props) => {
-  const { toggleModal, selectedPhoto, addFavourite } = props;
+  const { onClosePhotoDetailsModal, updateToFavPhotoIds, state, onPhotoSelect, photos } = props;
+  const handleCloseClick = () => {
+    onClosePhotoDetailsModal();
+  };
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={toggleModal}>
+      <button className="photo-details-modal__close-button" onClick={handleCloseClick}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
       {/* Photo Details */}
       <div className="photo-details-modal__images">
-        <PhotoFavButton addFavourite={() => addFavourite(selectedPhoto)}/>
-        {/* Because addFavourite is wrapped in arrow function, it does not immediatly call function, sending it on endless loop */}
-        <img src={selectedPhoto.urls.full} alt="Larger version of photo" className="photo-details-modal__image" />
+        <PhotoFavButton updateToFavPhotoIds={() => updateToFavPhotoIds(state.selectedPhoto)}/>
+         {/* Because updateToFavPhotoIds is wrapped in arrow function, it does not immediatly call function, sending it on endless loop */}
+        <img src={state.selectedPhoto.urls.full} alt="Larger version of photo" className="photo-details-modal__image" />
         {/* Photographer Details */}
         <div className='photo-details-modal__photographer-details'>
-          <img src={selectedPhoto.user.profile} alt="Profile picture" className="photo-details-modal__photographer-profile" />
+          <img src={state.selectedPhoto.user.profile} alt="Profile picture" className="photo-details-modal__photographer-profile" />
           <div className="photo-details-modal__photographer-info">
             <div className="photo-details-modal__photographer-name">
-              {selectedPhoto.user.name}
+              {state.selectedPhoto.user.name}
             </div>
             <div className="photo-details-modal__photographer-location">
-                {selectedPhoto.location.city}, {selectedPhoto.location.country}
+                {state.selectedPhoto.location.city}, {state.selectedPhoto.location.country}
             </div>
           </div>
         </div>
@@ -35,7 +38,7 @@ const PhotoDetailsModal = (props) => {
         <h2 className="photo-details-modal__header">Similar Photos:</h2>
       </div>
       {/* Replace the photos object in PhotoList with just the similar_photos. Use Object.values to iterate over the object's properties */}
-      <PhotoList photos={Object.values(selectedPhoto.similar_photos)} addFavourite={addFavourite} />
+      <PhotoList photos={Object.values(state.selectedPhoto.similar_photos)}updateToFavPhotoIds={updateToFavPhotoIds} onPhotoSelect={onPhotoSelect} />
     </div>
   )
 };
