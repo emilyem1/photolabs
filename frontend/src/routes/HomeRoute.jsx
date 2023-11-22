75a8b46d8d43147c540f4fc8348a82e2ec2cc7b7
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-21 21:37:03 -0800

    Pass down topics, that now uses data from API

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,10 +5,10 @@ import PhotoList from '../components/PhotoList';
 import TopNavigationBar from '../components/TopNavigationBar';
 
 const HomeRoute = (props) => {
-  const { state, onPhotoSelect, onLoadTopic, updateToFavPhotoIds, photos } = props;
+  const { state, onPhotoSelect, onLoadTopic, updateToFavPhotoIds, photos, topics } = props;
   return (
     <div className="home-route">
-      <TopNavigationBar onLoadTopic={onLoadTopic} state={state} isFavPhotoExist={state.favourite.length > 0}/>
+      <TopNavigationBar onLoadTopic={onLoadTopic} state={state} topics={topics} isFavPhotoExist={state.favourite.length > 0}/>
       <PhotoList 
         onPhotoSelect={onPhotoSelect}
         updateToFavPhotoIds={updateToFavPhotoIds}
