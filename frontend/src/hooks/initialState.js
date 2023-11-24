3e880515b1964ef10a727fedef47f07d29e3bf34
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-24 13:36:27 -0800

    Create initialState file to house all initial states to be used in useReducer

---
+++ frontend/src/hooks/initialState.js
@@ -0,0 +1,9 @@
+// The initialState to use in useApplicationData.js for useReducer
+
+export const initialState = {
+  selectedPhoto: null,
+  showModal: false,
+  favourite: [],
+  photoData: [],
+  topicData: []
+};
