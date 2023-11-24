a50653b9c74856600ef25d519fd5ff4705124e39
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-24 13:22:11 -0800

    Remove handle click, pass onPhotoSelect with arrow function

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -4,12 +4,9 @@ import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
   const { data, onLoadTopic } = props;
-  const handleClick = () => {
-    onLoadTopic(data);
-  };
   return (
     <div className="topic-list__item">
-      <span onClick={handleClick}>{data.title}</span>
+      <span onClick={() => onLoadTopic(data)}>{data.title}</span>
     </div>
   );
 };
