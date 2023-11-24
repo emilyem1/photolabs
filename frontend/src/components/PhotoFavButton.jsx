0102a4890c43f59e2c3367d6a666cf0a0bda0b05
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-24 13:17:23 -0800

    Change useState to boolean

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -5,15 +5,15 @@ import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
   const { updateToFavPhotoIds, data } = props;
-  const [fave, setFave] = useState("notFave");
+  const [fave, setFave] = useState(false);
   const handleClick = () => {
-    setFave(fave === "favourite" ? "notFave" : "favourite");
+    setFave(fave === true ? false : true);
     updateToFavPhotoIds(data);
   };
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg" onClick={handleClick}>
-          <FavIcon selected={fave === "favourite"}/>
+          <FavIcon selected={fave === true}/>
       </div>
     </div>
   );
