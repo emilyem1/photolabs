f0cbeac04a73ef536659cc14189850cbb53eb6e9
emilyem1 <emilyemcmahonn@gmail.com>
2023-11-24 21:39:59 -0800

    Rework useState for favourite, add useEffect to trigger a state update in favourite

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
-  const { updateToFavPhotoIds, data } = props;
-  const [fave, setFave] = useState(false);
-  const handleClick = () => {
-    setFave(fave === true ? false : true);
-    updateToFavPhotoIds(data);
-  };
+  const { updateToFavPhotoIds, data, favourite } = props;
+  // Check if the favourite array includes the data.id and sets the initial value of fave based on this condition:
+  const [fave, setFave] = useState(favourite.includes(data.id));
+  // useEffect triggers state update when favourite changes:
+  useEffect(() => {
+    setFave(favourite.includes(data.id));
+  }, [favourite]);
+
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg" onClick={handleClick}>
+      <div className="photo-list__fav-icon-svg" onClick={() => {updateToFavPhotoIds(data.id)}}>
           <FavIcon selected={fave === true}/>
       </div>
     </div>
